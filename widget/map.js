6e48d9aa2b071396e9f906a8ba2bbca458c3d837
Calculabs <calculabs@gmail.com>
2025-03-16 13:27:52 -0700

    simplify15

---
+++ widget/map.js
@@ -0,0 +1,13 @@
+// Question IDs that should be monitored for archetype selections
+export const QUESTION_IDS = [7, 9];
+
+// Fixed mapping of option indices to archetypes
+export const ARCHETYPE_MAP = {
+    '0': 'Atlas',
+    '1': 'Seer',
+    '2': 'Envoy',
+    '3': 'Nexus'
+};
+
+// Radio option indices (0-3 for each question)
+export const OPTION_INDICES = ['0', '1', '2', '3']; 
