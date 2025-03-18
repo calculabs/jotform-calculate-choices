3ffb73574e12ccaba4471a6c5791fd2952f17aad
Calculabs <calculabs@gmail.com>
2025-03-17 19:19:54 -0700

    Remove hardcoded QUESTION_IDS from map.js to streamline archetype selection handling

--- widget/map.js
+++ widget/map.js
@@ -1,6 +1,3 @@
-// Question IDs that should be monitored for archetype selections
-export const QUESTION_IDS = [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22];
-
 // Fixed mapping of option indices to archetypes
 export const ARCHETYPE_MAP = {
     '0': 'Atlas',
